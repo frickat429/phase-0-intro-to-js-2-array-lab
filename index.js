42aa57c84750ac6266f18c8b8028434d771c8fdc
Austin Frick <austin.frick429@gmail.com>
2023-08-26 17:44:48 -0400

    completelab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name)
+}
+
+function   destructivelyPrependCat(name) {
+    return cats.unshift(name)
+} 
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name) 
+}
+
+
+function appendCat(name) {
+    const coolCats = [...cats, name] 
+    return coolCats
+}
+
+function prependCat(name) {
+    const coldCats = [name, ...cats] 
+    return coldCats
+}
+
+function removeLastCat() { 
+    return cats.slice(0, cats.length - 1)
+}
+    
+function removeFirstCat() {
+    return cats.slice(1)  
+}
+
+
+  
